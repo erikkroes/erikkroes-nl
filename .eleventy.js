6122b663ccdcab3974709a4e625b12e24a5067a8
erik <erik.kroes@ing.com>
2021-02-19 20:51:55 +0100

    markdown IDs

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,19 @@
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+
 module.exports = function (eleventyConfig) {
-  
+
+  let markdownLibrary = markdownIt({ // turn headings into IDs
+    html: true,
+    breaks: true,
+    linkify: true
+  }).use(markdownItAnchor, {
+    permalink: true,
+    permalinkClass: "direct-link",
+    permalinkSymbol: "#"
+  });
+  eleventyConfig.setLibrary("md", markdownLibrary);
+
   // set copy asset folder to dist
   eleventyConfig.addPassthroughCopy('assets');
 
